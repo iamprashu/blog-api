6edc2aef28f58cba77ad35d45d0f3780c0106c02
iamprashu <iamprashu.gs@gmail.com>
2025-06-05 15:51:04 +0530

    Env File

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 
-const {connectDb} = require('./config/database')
+const {connectDb} = require('./config/database');
+const router = require('./routers/blogRoute');
+
 
 require('dotenv').config(); 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 
-
 //Routes
-
-
+app.use(router);
 //run app server
 app.listen(PORT,()=>{
     console.log(`Backend is Running on port ${PORT}`)
