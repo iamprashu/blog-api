63bb6d60e6f1ed595154c21bdfa14d7ee82caa57
iamprashu <iamprashu.gs@gmail.com>
2025-06-06 23:09:15 +0530

    Blog Router Updates

--- routers/blogRoute.js
+++ routers/blogRoute.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const postControllers = require("../controllers/postControllers");
 const createPost = postControllers.createPost;
+const getBlogs = postControllers.getPosts;
+
+const likesController = require('../controllers/likesController');
+const createLike = likesController.createLike;
 
 router.post('/post/create',createPost);
+router.get('/allBlogs',getBlogs);
 
-
+router.post('/like/create',createLike);
 
 module.exports = router;
