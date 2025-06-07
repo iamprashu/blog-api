8b332c1debc35e55d7c508726108c3694fe55f0f
iamprashu <iamprashu.gs@gmail.com>
2025-06-08 01:20:33 +0530

    routes m

--- routers/blogRoute.js
+++ routers/blogRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const postControllers = require("../controllers/postControllers");
 const createPost = postControllers.createPost;
 const getBlogs = postControllers.getPosts;
+const {signupController} = require('../controllers/signupController')
 
 const likesController = require('../controllers/likesController');
 const createLike = likesController.createLike;
@@ -12,5 +13,8 @@ router.post('/post/create',createPost);
 router.get('/allBlogs',getBlogs);
 
 router.post('/like/create',createLike);
+router.post('/user/new',createLike);
+
+router.post('/signup/new',signupController);
 
 module.exports = router;
