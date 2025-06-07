55b4d52d6e9f13789f3d6dc2b074a4814b8af04f
iamprashu <iamprashu.gs@gmail.com>
2025-06-08 01:19:52 +0530

    Complete Like Controller and Populate too

---
+++ controllers/likesController.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Like = require('../models/likeModel');
+const Post = require('../models/postModel');
+const bcrypt = require('bcrypt')
+
+exports.createLike = async(req,res)=>{
+
+    try{
+        const{post,user} = req.body;
+        
+        const user1 = await bcrypt.hash(user,10);
+
+
+        const newLike = new Like({
+            post,user:user1
+        })
+
+        const sentLike = await newLike.save();
+
+
+        const likedPost = await Post.findByIdAndUpdate(post,{$push: {likes:sentLike._id}},{new:true}).populate("likes");
+
+        res.status(200).json({
+            success:true,
+            data:likedPost,
+            message:"Liked Post Success"
+        })
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:"Something Went Wrong!"
+        })
+    }
+}
