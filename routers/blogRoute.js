cde8fcf64bcc217be4af76df4bb5e0efb5950b04
iamprashu <iamprashu.gs@gmail.com>
2025-06-10 22:21:26 +0530

    router module

--- routers/blogRoute.js
+++ routers/blogRoute.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const postControllers = require("../controllers/postControllers");
 const createPost = postControllers.createPost;
 const getBlogs = postControllers.getPosts;
-const {signupController} = require('../controllers/signupController')
-
+const {signupController, loginController} = require('../controllers/signupController')
+const {auth,isAdmin,isStudent} = require('../middlewares/auth');
 const likesController = require('../controllers/likesController');
 const createLike = likesController.createLike;
 
@@ -13,8 +13,28 @@ router.post('/post/create',createPost);
 router.get('/allBlogs',getBlogs);
 
 router.post('/like/create',createLike);
-router.post('/user/new',createLike);
 
-router.post('/signup/new',signupController);
+router.post('/signup',signupController);
+router.post('/login',loginController);
+
+
+//protected routes
+router.get('/simple',auth,(req,res)=>{
+    res.send("welcome")
+});
+
+router.get('/admin',auth,isAdmin,(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:'Welcome Admin'
+    })
+})
+
+router.get('/student',auth,isStudent,(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:'Welcome Student'
+    })
+})
 
 module.exports = router;
