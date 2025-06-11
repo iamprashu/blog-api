771fbd8251086e963d4f8297d07a9dfdf8c3d7d2
iamprashu <iamprashu.gs@gmail.com>
2025-06-12 00:28:54 +0530

    Like Controler Authentication User is identified by the Jwt + Cookies

--- routers/blogRoute.js
+++ routers/blogRoute.js
@@ -1,40 +1,46 @@
 const express = require('express');
 const router = express.Router();
 
+const likesController = require('../controllers/likesController');
+const createLike = likesController.createLike;
+
 const postControllers = require("../controllers/postControllers");
 const createPost = postControllers.createPost;
 const getBlogs = postControllers.getPosts;
-const {signupController, loginController} = require('../controllers/signupController')
-const {auth,isAdmin,isStudent} = require('../middlewares/auth');
-const likesController = require('../controllers/likesController');
-const createLike = likesController.createLike;
+
+const {signupController} = require('../controllers/signupController')
+const {loginController} = require('../controllers/loginController');
+const {auth} = require('../middlewares/auth');
+
+
 
 router.post('/post/create',createPost);
 router.get('/allBlogs',getBlogs);
 
-router.post('/like/create',createLike);
+router.post('/like',auth,createLike);
 
 router.post('/signup',signupController);
 router.post('/login',loginController);
 
 
-//protected routes
-router.get('/simple',auth,(req,res)=>{
-    res.send("welcome")
-});
 
-router.get('/admin',auth,isAdmin,(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:'Welcome Admin'
-    })
-})
+// //protected routes
+// router.get('/simple',auth,(req,res)=>{
+//     res.send("welcome")
+// });
 
-router.get('/student',auth,isStudent,(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:'Welcome Student'
-    })
-})
+// router.get('/admin',auth,isAdmin,(req,res)=>{
+//     res.status(200).json({
+//         success:true,
+//         message:'Welcome Admin'
+//     })
+// })
+
+// router.get('/student',auth,isStudent,(req,res)=>{
+//     res.status(200).json({
+//         success:true,
+//         message:'Welcome Student'
+//     })
+// })
 
 module.exports = router;
